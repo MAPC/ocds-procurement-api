fb6292855efc7d0c19dcc95cd48e70fedc5bac21
Brad Smith <brad@smithwebtek.com>
2019-11-01 12:09:31 -0400

    Change app name and related refs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_graphql_v1_production"
+  # config.active_job.queue_name_prefix = "open_data_demo_production"
 
   config.action_mailer.perform_caching = false
 
