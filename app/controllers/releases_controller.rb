1cd11c6aaf3e7369ec5604d5bb187837b1753f3c
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-11-05 15:46:38 -0500

    Add Test for Validating Releases
    * Add test for invalid data submissions on POST
    * Add validation of JSON to model

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,11 +1,11 @@
 class ReleasesController < ApplicationController
   def create
     package = Package.find_or_create_by!(uri: "#{request.base_url}/packages/#{params['ocid']}")
-    release = Release.new(data: params.to_json, package: package)
+    release = Release.new(data: params, package: package)
     if release.save
       render json: '{ "status" => "worked" }', status: :created
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: release.errors, status: :unprocessable_entity
     end
   end
 end
--- app/models/release.rb
+++ app/models/release.rb
@@ -1,3 +1,21 @@
 class Release < ApplicationRecord
   belongs_to :package
+  validate :check_release_integrity
+
+  def check_release_integrity
+    schema_directory = "#{Dir.pwd}/spec/support/api/schemas"
+    schema_path = "#{schema_directory}/single-release.json"
+
+    if JSON::Validator.validate(schema_path, self.data, strict: true)
+      return true
+    elsif JSON::Validator.fully_validate(schema_path, self.data, strict: true)
+                         .select { |value| value.include?("contained undefined properties") }
+                         .present?
+      errors.add(:data, :invalid, message: JSON::Validator.fully_validate(schema_path, self.data, strict: true).select { |value| value.include?("contained undefined properties") })
+    else
+      unless JSON::Validator.validate(schema_path, self.data)
+        errors.add(:data, :invalid, message: JSON::Validator.fully_validate(schema_path, self.data))
+      end
+    end
+  end
 end
