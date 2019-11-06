bb3e64153c8365336190308dbb0675b7186f7de5
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-11-06 14:10:07 -0500

    Minimal JSON Release Test
    Add a minimal JSON release to test the API receiving a release from a Google form with the minimum number of required fields.
    
    Add config.hosts = nil to fix rails blocked host working with ngrok.
    
    Add automated test for minimal JSON submission.
    
    Co-authored-by: Brad Smith <brad@smithwebtek.com>

--- spec/requests/api/v1/releases_spec.rb
+++ spec/requests/api/v1/releases_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe "validate release", type: :request do
   it "saves a POSTed valid release" do
     json = JSON.parse(file_fixture("release_valid_v1_1_0.json").read)
     post '/releases', params: { data: json }, as: :json
-    expect(response).to have_http_status(:created)
+    expect(response).to have_http_status(:created), JSON.parse(response.body)['data']
   end
 
   it "rejects a POSTed invalid release" do
     json = JSON.parse(file_fixture("release_invalid_v1_1_0.json").read)
     post '/releases', params: { data: json }, as: :json
-    expect(response).to have_http_status(:unprocessable_entity)
+    expect(response).to have_http_status(:unprocessable_entity), JSON.parse(response.body)['data']
+  end
+
+  it "accepts a minimal JSON release" do
+    json = JSON.parse(file_fixture("release_minimal_v1_1_0.json").read)
+    post '/releases', params: { data: json }, as: :json
+    expect(response).to have_http_status(:created), JSON.parse(response.body)['data']
   end
 end
